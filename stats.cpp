5d26fe32a7dd6451e5a6b111754e4b09fc6c9c71
Anjali Kumari <37630174+Anjalikumari15@users.noreply.github.com>
2025-04-24 17:45:09 +0530

    Define ComputeStatistics function

--- stats.cpp
+++ stats.cpp
@@ -2,4 +2,19 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
     //Implement statistics here
+    Stats result;
+    if(v.size()>=1) {
+        result.average = v.at(0);
+        result.min = v.at(0);
+        result.max = v.at(0);
+    }
+    for (int num : v) {
+        result.average += num
+        if(num<result.min)
+            result.min = num;
+        if(num>result.max)
+            result.max = num;
+    }
+    result.average = (float) result.average/v.size();
+    return res;
 }
